058006c4bc30a1dfb298ba8c141dbf2bf0b984a4
shurpalisameer <sameershurpali07@gmail.com>
2024-01-17 14:21:31 +0530

    My first commit

---
+++ POJO_Class.java
@@ -0,0 +1,25 @@
+public class Serialization
+{
+ private String messege;
+ private String Name;
+ 
+ public String Setmessage(String message)
+ {
+	 this.message = message;
+ }
+ public void getmessage()
+ {
+	 return message;
+ }
+ public String SetName(String Name)
+ {
+	 this.Name = Name;
+ }
+ public void getName()
+ {
+     return Name;
+ }
+ 
+}
+ 
+ 
